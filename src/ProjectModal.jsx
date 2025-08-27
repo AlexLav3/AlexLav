aaa6e7b5fdaab7e1be06b00e319894f76a2d8eb2
Alex Lav <alexlav.elly@gmail.com>
2025-08-27 02:07:05 +0200

    fix 2

--- src/ProjectModal.jsx
+++ src/ProjectModal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectModal = ({ project, onClose, onBack }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
-      <div className="bg-background dark:bg-gray-900 rounded-lg p-6 max-w-md w-full shadow-lg">
+      <div className="bg-background rounded-lg p-6 max-w-md w-full shadow-lg">
         <h2 className="text-xl font-bold mb-2">{project.name}</h2>
         <img src={project.img} className="h-50 w-full rounded-md mb-4" />
         <p className="mb-4">{project.description}</p>
